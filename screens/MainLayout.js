e2c88a7d32cabe2a71312926ecddae2409d86554
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-10-06 00:00:54 +0300

    BottomTabMenu v1

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -4,7 +4,8 @@ import {
     Text,
     Image,
     SafeAreaView,
-    TouchableOpacity
+    TouchableOpacity,
+    TouchableWithoutFeedback
 } from 'react-native';
 
 import {
@@ -24,6 +25,58 @@ import Animated, {
 import { connect } from 'react-redux'
 import { setSelectedTab } from '../stores/tab/tabActions'
 import { Header } from '../components'
+import { LinearGradient } from 'expo-linear-gradient';
+
+const TabButton = ({ label, icon, isFocused, onPress }) => {
+    return (
+        <TouchableWithoutFeedback
+            onPress={onPress}
+        >
+            <Animated.View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: COLORS.white,
+                }}
+            >
+                <Animated.View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        width: '80%',
+                        height: 50,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: 25
+                    }}
+                >
+                    <Image
+                        source={icon}
+                        style={{
+                            width: 20,
+                            height: 20,
+                            tintColor: COLORS.gray
+                        }}
+                    />
+
+                    {isFocused &&
+                        <Text
+                            numberOfLines={1}
+                            style={{
+                                marginLeft: SIZES.base,
+                                color: COLORS.gray,
+                                ...FONTS.h3
+                            }}
+                        >
+                            {label}
+                        </Text>
+                    }
+                </Animated.View>
+            </Animated.View>
+        </TouchableWithoutFeedback>
+    )
+}
 
 const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelectedTab }) => {
 
@@ -35,9 +88,7 @@ const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelected
         <Animated.View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'white',
+                backgroundColor: COLORS.white,
                 ...drawerAnimationStyle
             }}
         >
@@ -66,6 +117,25 @@ const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelected
                         <Image source={icons.menu} />
                     </TouchableOpacity>
                 }
+                rightComponent={
+                    <TouchableOpacity
+                        style={{
+                            borderRadius: SIZES.radius,
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <Image
+                            source={dummyData?.myProfile?.profile_image}
+                            style={{
+                                width: 40,
+                                height: 40,
+                                borderRadius: SIZES.radius
+                            }}
+                        />
+
+                    </TouchableOpacity>
+                }
             />
 
             {/* Content */}
@@ -78,6 +148,80 @@ const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelected
             </View>
 
             {/* Footer */}
+            <View
+                style={{
+                    height: 100,
+                    justifyContent: 'center'
+                }}
+            >
+                {/* Shadow */}
+                <LinearGradient
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 0, y: 3 }}
+                    colors={[
+                        COLORS.transparent,
+                        COLORS.black
+                    ]}
+                    style={{
+                        position: 'absolute',
+                        top: -20,
+                        left: 0,
+                        right: 0,
+                        height: 100,
+                        borderTopLeftRadius: 15,
+                        borderTopRightRadius: 15
+                    }}
+                />
+
+                {/* Tabs */}
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        paddingHorizontal: SIZES.radius,
+                        paddingBottom: 10,
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                        backgroundColor: COLORS.white
+                    }}
+                >
+                    <TabButton
+                        label={constants.screens.home}
+                        icon={icons.home}
+                        isFocused={selectedTab == constants.screens.home}
+                        onPress={() => setSelectedTab(constants.screens.home)}
+                    />
+
+                    <TabButton
+                        label={constants.screens.search}
+                        icon={icons.search}
+                        isFocused={selectedTab == constants.screens.search}
+                        onPress={() => setSelectedTab(constants.screens.search)}
+                    />
+
+                    <TabButton
+                        label={constants.screens.cart}
+                        icon={icons.cart}
+                        isFocused={selectedTab == constants.screens.cart}
+                        onPress={() => setSelectedTab(constants.screens.cart)}
+                    />
+
+                    <TabButton
+                        label={constants.screens.favourite}
+                        icon={icons.favourite}
+                        isFocused={selectedTab == constants.screens.favourite}
+                        onPress={() => setSelectedTab(constants.screens.favourite)}
+                    />
+
+                    <TabButton
+                        label={constants.screens.notification}
+                        icon={icons.notification}
+                        isFocused={selectedTab == constants.screens.notification}
+                        onPress={() => setSelectedTab(constants.screens.notification)}
+                    />
+                </View>
+
+            </View>
 
         </Animated.View>
     )
