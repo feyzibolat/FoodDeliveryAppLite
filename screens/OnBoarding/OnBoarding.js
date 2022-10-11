28492079d6c740098e51db5e7a020bfbc63bc620
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-10-11 20:49:16 +0300

    Update OnBoarding.js

--- screens/OnBoarding/OnBoarding.js
+++ screens/OnBoarding/OnBoarding.js
@@ -7,8 +7,49 @@ import {
     Animated
 } from 'react-native';
 import { constants, images, FONTS, SIZES, COLORS } from "../../constants";
+import { TextButton } from "../../components";
 
-const OnBoarding = () => {
+const OnBoarding = ({ navigation }) => {
+
+    const scrollX = React.useRef(new Animated.Value(0)).current
+    const flatListRef = React.useRef()
+
+    const [currentIndex, setCurrentIndex] = React.useState(0)
+    const onViewChangeRef = React.useRef(({ viewableItems, changed }) => setCurrentIndex(viewableItems[0].index))
+
+    const Dots = () => {
+        const dotPosition = new Animated.divide(scrollX, SIZES.width)
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                {
+                    constants.onboarding_screens.map((item, index) => {
+                        const dotColor = dotPosition.interpolate({ inputRange: [index - 1, index, index + 1], outputRange: [COLORS.lightOrange, COLORS.primary, COLORS.lightOrange], extrapolate: 'clamp' })
+
+                        const dotWidth = dotPosition.interpolate({ inputRange: [index - 1, index, index + 1], outputRange: [10, 30, 10], extrapolate: 'clamp' })
+
+                        return (
+                            <Animated.View
+                                key={`dot-${index}`}
+                                style={{
+                                    borderRadius: 5,
+                                    marginHorizontal: 6,
+                                    width: dotWidth,
+                                    height: 10,
+                                    backgroundColor: dotColor
+                                }}
+                            />
+                        )
+                    })
+                }
+            </View>
+        )
+    }
 
     function renderHeaderLogo() {
         return (
@@ -38,10 +79,74 @@ const OnBoarding = () => {
         return (
             <View
                 style={{
-                    height: 160
+                    height: SIZES.height / 10
                 }}
             >
+                {/* Pagination / Dots */}
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Dots />
+                </View>
 
+                {/* Buttons */}
+                {currentIndex < constants.onboarding_screens.length - 1 &&
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            paddingHorizontal: SIZES.padding,
+                            marginVertical: SIZES.padding
+                        }}
+                    >
+                        <TextButton
+                            label='Skip'
+                            buttonContainerStyle={{
+                                backgroundColor: null
+                            }}
+                            labelStyle={{
+                                color: COLORS.darkGray2
+                            }}
+                            onPress={() => navigation.replace('SignIn')}
+                        />
+
+                        <TextButton
+                            label='Next'
+                            buttonContainerStyle={{
+                                height: 60,
+                                width: 200,
+                                borderRadius: SIZES.radius
+                            }}
+                            onPress={() => {
+                                flatListRef?.current?.scrollToIndex({
+                                    index: currentIndex + 1,
+                                    animated: true
+                                })
+                            }}
+                        />
+                    </View>
+                }
+
+                {currentIndex == constants.onboarding_screens.length - 1 &&
+                    <View
+                        style={{
+                            paddingHorizontal: SIZES.padding,
+                            marginVertical: SIZES.padding
+                        }}
+                    >
+                        <TextButton
+                            label="Let's Get Started"
+                            buttonContainerStyle={{
+                                height: 60,
+                                borderRadius: SIZES.radius
+                            }}
+                            onPress={() => navigation.replace('SignIn')}
+                        />
+                    </View>
+                }
             </View>
         )
     }
@@ -56,29 +161,40 @@ const OnBoarding = () => {
             {renderHeaderLogo()}
 
             <Animated.FlatList
+                ref={flatListRef}
                 horizontal
                 pagingEnabled
                 data={constants.onboarding_screens}
                 scrollEventThrottle={16}
                 snapToAlignment='center'
                 showsHorizontalScrollIndicator={false}
+                onScroll={Animated.event(
+                    [
+                        {
+                            nativeEvent: { contentOffset: { x: scrollX } }
+                        }
+                    ],
+                    { useNativeDriver: false }
+                )}
+                onViewableItemsChanged={onViewChangeRef.current}
                 keyExtractor={item => `${item.id}`}
                 renderItem={({ item, index }) => {
                     return (
                         <View
                             style={{
-                                width: SIZES.width
+                                width: SIZES.width,
                             }}
                         >
                             {/* Header */}
                             <ImageBackground
                                 source={item.backgroundImage}
+                                resizeMode='stretch'
                                 style={{
                                     flex: 1,
                                     alignItems: 'center',
                                     justifyContent: 'flex-end',
-                                    height: '100%',
-                                    width: '100%'
+                                    height: index == 1 ? '86%' : '100%',
+                                    width: '100%',
                                 }}
                             >
                                 <Image
@@ -96,7 +212,7 @@ const OnBoarding = () => {
                             <View
                                 style={{
                                     flex: 1,
-                                    marginTop: 0,
+                                    marginTop: -150,
                                     alignItems: 'center',
                                     justifyContent: 'center',
                                     paddingHorizontal: SIZES.radius
