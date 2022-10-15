d7d27a8bb98df384117ddd098f45f709f49d7f1a
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-10-15 19:01:20 +0300

    Otp & ForgotPassword Added

--- screens/Authentication/ForgotPassword.js
+++ screens/Authentication/ForgotPassword.js
@@ -1,14 +1,83 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    Image
 } from 'react-native';
 
-const ForgotPassword = () => {
+import { AuthLayout } from '../'
+import { FONTS, SIZES, COLORS, icons } from '../../constants'
+import { FormInput, TextButton } from '../../components'
+import { utils } from '../../utils'
+
+const ForgotPassword = ({ navigation }) => {
+
+    const [email, setEmail] = React.useState("")
+    const [emailError, setEmailError] = React.useState("")
+
+    function isEnableSendEmail() {
+        return email != "" && emailError == ""
+    }
+
     return (
-        <View>
-            <Text>ForgotPassword</Text>
-        </View>
+        <AuthLayout
+            title="Password Recovery"
+            subtitle="Please enter your email address to recover your password"
+            titleContainerStyle={{
+                marginTop: SIZES.padding * 2
+            }}
+        >
+            {/* Form Input */}
+            <View
+                style={{
+                    flex: 1,
+                    marginTop: SIZES.padding * 2
+                }}
+            >
+                <FormInput
+                    label="Email"
+                    keyboardType="email-address"
+                    autoCompleteType="email"
+                    onChange={(value) => {
+                        utils.validateEmail(value, setEmailError)
+                        setEmail(value)
+                    }}
+                    errorMsg={emailError}
+                    appendComponent={
+                        <View
+                            style={{
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Image
+                                source={email == "" || (email != "" && emailError == "") ? icons.correct : icons.cancel}
+                                style={{
+                                    height: 20,
+                                    width: 20,
+                                    tintColor: email == "" ? COLORS.gray : (email != "" && emailError == "") ? COLORS.green : COLORS.red
+                                }}
+                            />
+                        </View>
+                    }
+                />
+            </View>
+
+            {/* Button */}
+            <TextButton
+                label="Send Email"
+                disabled={isEnableSendEmail() ? false : true}
+                buttonContainerStyle={{
+                    height: 55,
+                    alignItems: 'center',
+                    marginTop: SIZES.padding,
+                    borderRadius: SIZES.radius,
+                    backgroundColor: isEnableSendEmail() ? COLORS.primary : COLORS.transparentPrimary
+                }}
+                onPress={() => navigation.goBack()}
+            />
+
+        </AuthLayout>
     )
 }
 
--- screens/Authentication/Otp.js
+++ screens/Authentication/Otp.js
@@ -4,11 +4,132 @@ import {
     Text
 } from 'react-native';
 
+import OTPInputView from "@twotalltotems/react-native-otp-input";
+import { AuthLayout } from '../'
+import { FONTS, SIZES, COLORS } from '../../constants'
+import {
+    TextButton,
+} from '../../components'
 const Otp = () => {
+
+    const [timer, setTimer] = React.useState(60)
+
+    React.useEffect(() => {
+        let interval = setInterval(() => {
+            setTimer(prevTimer => {
+                if (prevTimer > 0) {
+                    return prevTimer - 1
+                } else {
+                    return prevTimer
+                }
+            })
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [])
+
     return (
-        <View>
-            <Text>Otp</Text>
-        </View>
+        <AuthLayout
+            title="OTP Authentication"
+            subtitle="An authentication code has been sent to byprogrammers@gmail.com"
+            titleContainerStyle={{
+                marginTop: SIZES.padding * 2
+            }}
+        >
+            {/* OTP inputs */}
+            <View
+                style={{
+                    flex: 1,
+                    marginTop: SIZES.padding * 2
+                }}
+            >
+
+                <OTPInputView
+                    pinCount={4}
+                    style={{
+                        width: '100%',
+                        height: 50
+                    }}
+                    codeInputFieldStyle={{
+                        width: 65,
+                        height: 65,
+                        borderRadius: SIZES.radius,
+                        backgroundColor: COLORS.lightGray2,
+                        color: COLORS.black,
+                        ...FONTS.h3
+                    }}
+                    onCodeFilled={(code) => {
+                        console.log(code)
+                    }}
+                />
+
+                {/* Countdown Timer */}
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        marginTop: SIZES.padding
+                    }}
+                >
+                    <Text style={{ color: COLORS.darkGray, ...FONTS.body3 }}>Didn't receive code?</Text>
+
+                    <TextButton
+                        label={`Resend (${timer}s)`}
+                        disabled={timer == 0 ? false : true}
+                        buttonContainerStyle={{
+                            marginLeft: SIZES.base,
+                            backgroundColor: null
+                        }}
+                        labelStyle={{
+                            color: COLORS.primary,
+                            ...FONTS.h3
+                        }}
+                        onPress={() => setTimer(60)}
+                    />
+                </View>
+            </View>
+
+            {/* Footer */}
+            <View>
+                <TextButton
+                    label="Continue"
+                    buttonContainerStyle={{
+                        height: 50,
+                        alignItems: 'center',
+                        borderRadius: SIZES.radius,
+                        backgroundColor: COLORS.primary
+                    }}
+                    onPress={() => console.log("Continue")}
+                />
+
+                <View
+                    style={{
+                        marginTop: SIZES.padding,
+                        alignItems: 'center'
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: COLORS.darkGray,
+                            ...FONTS.body3
+                        }}
+                    >
+                        By signing up, you agree to our.
+                    </Text>
+                    <TextButton
+                        label="Terms and Conditions"
+                        buttonContainerStyle={{
+                            backgroundColor: null
+                        }}
+                        labelStyle={{
+                            color: COLORS.primary,
+                            ...FONTS.h3
+                        }}
+                        onPress={() => console.log("TnC")}
+                    />
+                </View>
+            </View>
+        </AuthLayout>
     )
 }
 
