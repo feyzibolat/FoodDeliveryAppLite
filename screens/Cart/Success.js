01ff421f28c3eaa57b66fad6d8cb430c894d45a1
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-10-29 12:56:56 +0300

    Update Success.js

--- screens/Cart/Success.js
+++ screens/Cart/Success.js
@@ -1,18 +1,78 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    Image,
+    BackHandler
 } from 'react-native';
 
+import {
+    TextButton
+} from '../../components'
+
+import { FONTS, SIZES, COLORS, images } from '../../constants'
+
 const Success = ({ navigation }) => {
+
+    React.useEffect(() => {
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', () => { return true })
+
+        return () => backHandler.remove();
+    })
 
     return (
         <View
             style={{
-                flex: 1
+                flex: 1,
+                paddingHorizontal: SIZES.padding,
+                backgroundColor: COLORS.white
             }}
         >
-            <Text>Success</Text>
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Image
+                    source={images.success}
+                    resizeMode='contain'
+                    style={{
+                        width: 150,
+                        height: 150,
+                    }}
+                />
+
+                <Text
+                    style={{
+                        marginTop: SIZES.padding,
+                        ...FONTS.h1
+                    }}
+                >
+                    Congratulations!
+                </Text>
+
+                <Text style={{
+                    textAlign: 'center',
+                    marginTop: SIZES.base,
+                    color: COLORS.darkGray,
+                    ...FONTS.body3
+                }}>
+                    Payment was successfully made!
+                </Text>
+            </View>
+
+            <TextButton
+                label="Done"
+                buttonContainerStyle={{
+                    height: 55,
+                    marginBottom: SIZES.padding,
+                    borderRadius: SIZES.radius,
+                    backgroundColor: COLORS.primary
+                }}
+                onPress={() => navigation.navigate("DeliveryStatus")}
+            />
         </View>
     )
 }
